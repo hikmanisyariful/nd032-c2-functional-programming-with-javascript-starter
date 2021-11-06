dac870d2817530f830302b712b9ae359b1ad745e
hikmanisyariful <syarifulfajar017@gmail.com>
2021-11-06 08:29:53 +0700

    Fetch mars rovers API

--- project/src/public/client.js
+++ project/src/public/client.js
@@ -1,105 +1,153 @@
-let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+let store = Immutable.Map({
+  user: { name: "Student" },
+  apod: "",
+  rovers: ["Curiosity", "Opportunity", "Spirit"],
+  display: false
+});
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
-const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+const updateStore = (state, newState) => {
+  store = state.merge(newState);
+  render(root, store);
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
-
+  root.innerHTML = App(state);
+};
 
 // create content
-const App = (state) => {
-    let { rovers, apod } = state
-
+const NavigationRover = rovers => {
+  return rovers.map(rover => {
     return `
-        <header></header>
+                <div class="rover">
+                    <button class="btn-nav" type="button" id="${rover}" onclick="handleClick(${rover})">
+                        ${rover}                   
+                    </button>
+                </div>
+            `;
+  });
+};
+
+const handleClick = button => {
+  const selectedRover = button.id;
+  getDataRover(selectedRover);
+};
+
+const App = state => {
+  const rovers = state.get("rovers");
+
+  return `
+        <header>
+            <div>
+                <h1>Galery of Mars Rovers</h1>
+                <h4>Please, choose which rover’s information you want to see</h4>
+            <div>
+            <nav class="rover-nav">
+                ${NavigationRover(rovers)}
+            </nav>
+        </header>
         <main>
-            ${Greeting(store.user.name)}
             <section>
-                <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
+                <div>
+                </div>
             </section>
         </main>
         <footer></footer>
-    `
-}
+    `;
+};
 
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store)
-})
+window.addEventListener("load", () => {
+  render(root, store);
+});
 
 // ------------------------------------------------------  COMPONENTS
 
-// Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
+// 1. Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
 
+/*
+const Greeting = name => {
+  if (name) {
     return `
+            <h1>Welcome, ${name}!</h1>
+        `;
+  }
+
+  return `
         <h1>Hello!</h1>
-    `
-}
+    `;
+};
+*/
 
-// Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
+// 2. Example of a pure function that renders infomation requested from the backend
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = new Date(apod.date)
-    console.log(photodate.getDate(), today.getDate());
+/*
+const ImageOfTheDay = apod => {
+  // If image does not already exist, or it is not from today -- request it again
+  const today = new Date();
+  const photodate = new Date(apod.date);
+  console.log(photodate.getDate(), today.getDate());
 
-    console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    }
+  console.log(photodate.getDate() === today.getDate());
+  if (!apod || apod.date === today.getDate()) {
+    getImageOfTheDay(store);
+  }
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
+  // check if the photo of the day is actually type video!
+  if (apod.media_type === "video") {
+    return `
             <p>See today's featured video <a href="${apod.url}">here</a></p>
             <p>${apod.title}</p>
             <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
+        `;
+  } else {
+    return `
             <img src="${apod.image.url}" height="350px" width="100%" />
             <p>${apod.image.explanation}</p>
-        `)
-    }
-}
+        `;
+  }
+};
+*/
 
 // ------------------------------------------------------  API CALLS
 
-// Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+// 3. Example API call
+const getDataRover = roverName => {
+  fetch(`http://localhost:3000/rover/${roverName}`)
+    .then(res => res.json())
+    .then(({ data }) => {
+      const lastIdCamera = (acc, curr) => {
+        if (curr.camera.name === acc.camera.name) {
+          if (curr.id > acc.id) {
+            return curr;
+          }
+        }
+        return acc;
+      };
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => updateStore(store, { apod }))
+      const getLatesPhotosEachCamera = (acc, curr, i, arr) => {
+        acc[curr.camera.name] =
+          acc[curr.camera.name] === undefined
+            ? curr
+            : lastIdCamera(acc[curr.camera.name], curr);
 
-    return data
-}
+        if (i + 1 === arr.length) {
+          return Object.values(acc);
+        }
+        return acc;
+      };
+
+      const cameraEachRover = data.photos.reduce(getLatesPhotosEachCamera, {});
+      console.log("Ini Data", cameraEachRover);
+    });
+};
+
+/*
+const getImageOfTheDay = () => {
+  fetch(`http://localhost:3000/apod`)
+    .then(res => res.json())
+    .then(apod => updateStore(store, { apod }));
+};
+*/
